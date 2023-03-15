9f8caa983dd550730c258250dd20d13534cdefb7
Patrick Swartz <patrick.swartz@suse.com>
2023-03-15 12:12:48 -0500

    Update config.sh
    Added setting hostname

--- OSImage/Image-sle15sp3-JeOS/config.sh
+++ OSImage/Image-sle15sp3-JeOS/config.sh
@@ -92,6 +92,16 @@ USERCONTROL='no'
 EOF
 
 #======================================
+# Set Hostname
+#--------------------------------------
+HOST_PREFIX=${HOST_PREFIX:-"phsvm"}
+NET_DEVICE=${NET_DEVICE:"eth0"}
+LAST_MAC4=${sed -rn "s/^.*([0-9A-F:]{5})$/\1/gi;s/://p" /sys/class/net/${NET_DEVICE}/address
+NEW_HOSTNAME=${HOST_PREFIX}-{$LAST_MAC4:-0000}
+echo $NEW_HOSTNAME > /etc/hostname
+/bin/hostname -F /etc/hostname
+
+#======================================
 # Enable sshd
 #--------------------------------------
 chkconfig sshd on
